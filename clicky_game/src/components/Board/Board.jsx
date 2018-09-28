6e29b9225a5d8f668982499280dbd7c31e1b0e7a
chibimayura <chibimayura@gmail.com>
2018-09-27 20:40:40 -0700

    template for clicky memory game

---
+++ clicky_game/src/components/Board/Board.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Board.css';
+
+class Board extends Component {
+	render() {
+		return (
+		  <div className="Board">
+				<img src="https://consequenceofsound.files.wordpress.com/2017/04/homer-simpson-feature1.png?w=807" alt="Homer"/>
+		  </div>
+		);
+	  }
+}
+
+export default Board;
